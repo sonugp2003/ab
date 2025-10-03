72a4a8fe214174da6c1dac34b118e86ec69ecbec
SONU GUPTA <aloksonug2006@gmail.com>
2025-10-03 10:40:38 +0000

    Try fixing this error: `Console Error: Error: can't access property Symb

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,6 @@
+
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, getDoc } from "firebase/firestore";
 
@@ -15,8 +16,8 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase for SSR
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
