8c0add27cc2eb9aebd3ade4a35c3ea0c669f4286
Alok Ranjan <cse24011@iiitkalyani.ac.in>
2025-10-04 00:08:29 +0530

    Delete src directory

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "roomlogger-66325400-eccb5",
-  "appId": "1:841886122773:web:d737e8e393bdea5a2c9f1c",
-  "apiKey": "AIzaSyDhpShAwY_IuENuGSBXzGaRYYRLNSEyAt8",
-  "authDomain": "roomlogger-66325400-eccb5.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "841886122773"
-};
